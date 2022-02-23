8f0b66f66f229311d3f69c4de5a089d148757933
beatrizsibilio <beatrizduartesibilio@gmail.com>
2022-02-23 14:28:28 -0300

    criando sistema de template

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,50 +1,4 @@
-<!--início do header-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Seja bem vindo!</h1>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-        <!--fim do header-->
         <!--início do corpo da página-->
         <div class="row">
             <div class="col">
@@ -57,7 +11,7 @@
                         <img src="https://guiaviajarmelhor.com.br/wp-content/uploads/2019/11/melhor-praia-do-mundo-tripadvisor-5.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                        <img src="https://images.tcdn.com.br/img/img_prod/650565/papel_de_parede_3d_praias_0020_adesivo_paisagem_1673_2_20201007172046.jpg" class="d-block w-100" alt="...">
+                        <img src="https://www.costacruzeiros.com/content/dam/costa/costa-magazine/articles-magazine/beaches/barbados-beaches/barbados-spiagge_m.jpg.image.694.390.low.jpg">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -72,9 +26,4 @@
             </div>
         </div>
         <!--fim do corpo da página-->
-        <!--início do footer-->
-
-    </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+        
